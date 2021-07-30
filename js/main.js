fd42dcdc1587a7b2be991df1217fd66584578fe6
Joan <no7au53r@gmail.com>
2021-07-30 12:12:13 +0300

    hw01 done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,32 @@
 // 
 
-const isChecked = true,
+/* const isChecked = true,
     isClose = true;
 
-    console.log(isChecked && isClose)
+    console.log(isChecked && isClose) */
+
+
+   // ============================================
+               
+   let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', 'Сколько фильмов вы уже посмотрели?')
+ 
+const personalMovieDB = {
+    'count' : numberOfFilms,
+    'movies' : {},
+    'actors' : {},
+    'genres' : [],
+    'privat' :  false
+    
+}
+     
+
+
+
+let movies = {}
+
+const moviesFilm = prompt('Один из последних просмотренных фильмов?'),
+    moviesCount = prompt('На сколько оцените его?');
+
+personalMovieDB.movies[moviesFilm] = moviesCount
+ 
+console.log(personalMovieDB)
